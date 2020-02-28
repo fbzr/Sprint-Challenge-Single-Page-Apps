43f131b5c78504bb10d41bf5796f543162a2b79d
fbzr <fabricio.spb@gmail.com>
2020-02-28 08:27:27 -0800

    create routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import { Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Route exact path='/'>
+        <WelcomePage />
+      </Route>
+      <Route exact path='/characters'>
+        <CharacterList />
+      </Route>
     </main>
   );
 }
