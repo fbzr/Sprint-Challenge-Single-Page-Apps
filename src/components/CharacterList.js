07f2f1baa02ca6c7febb66a5a6ca454ad4e90153
fbzr <fabricio.spb@gmail.com>
2020-02-28 08:25:41 -0800

    fetch data from api

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
+    Axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
